2baa17bbcfb69db33bc614c2f1bf3ae211767c09
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-02-14 23:41:24 +0600

    Hsc Ict app tamplate created

---
+++ app/src/main/java/com/raihan/hscict/MainActivity.java
@@ -0,0 +1,27 @@
+package com.raihan.hscict;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Handler h = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               Intent i = new Intent(MainActivity.this,home.class);
+startActivity(i);
+finish();
+
+}       },5000);
+    }
+}
+
+
+
